2842b8e3042c7ba911a1f8aa986c0577a6df8a05
Mateen <mateendev3@gmail.com>
2022-09-25 20:16:08 +0500

    Added utils and changed main.dart file

---
+++ lib/utils/colors_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backgroundColor = Color(0xFFFFFFFF);
+  static const textColor = Color(0xFF000000);
+  static const appBarColor = Color(0xFFFB7F6B);
+  static const searchBarColor = Color(0xFFFB7F6B);
+  static const messageColor = Color(0xFFFB7F6B);
+}
