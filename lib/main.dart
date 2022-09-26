e944b5acef9b04e199f706061c3b56dc4503cb55
Mateen <mateendev3@gmail.com>
2022-09-26 12:58:19 +0500

    Tested app with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:lets_chat/firebase_options.dart';
 import 'package:lets_chat/utils/string_constants.dart';
 import 'router.dart';
 import 'utils/theme_constants.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
--- lib/utils/info.dart
+++ lib/utils/info.dart
@@ -4,70 +4,70 @@ const info = [
     'message': 'Hey, how are you doing?',
     'time': '3:53 pm',
     'profilePic':
-        'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
+        'https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg'
   },
   {
     'name': 'John Doe',
     'message': 'Hello, whats up',
     'time': '2:25 pm',
     'profilePic':
-        'https://www.socialketchup.in/wp-content/uploads/2020/05/fi-vill-JOHN-DOE.jpg',
+        'https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg'
   },
   {
     'name': 'Naman Ranawat',
     'message': 'Hello, I want to sleep.',
     'time': '1:03 pm',
     'profilePic':
-        'https://media.cntraveler.com/photos/60596b398f4452dac88c59f8/16:9/w_3999,h_2249,c_limit/MtFuji-GettyImages-959111140.jpg',
+        'https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg'
   },
   {
     'name': 'Dad',
     'message': 'Call me, have some work',
     'time': '12:06 pm',
     'profilePic':
-        'https://pbs.twimg.com/profile_images/1419974913260232732/Cy_CUavB.jpg',
+        'https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg'
   },
   {
     'name': 'Mom',
     'message': 'You ate food?',
     'time': '10:00 am',
     'profilePic':
-        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+        'https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg'
   },
   {
     'name': 'Jurica',
     'message': 'Yo!!!!! Long time, no see!?',
     'time': '9:53 am',
     'profilePic':
-        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+        'https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg'
   },
   {
     'name': 'Albert Dera',
     'message': 'Am I fat?',
     'time': '7:25 am',
     'profilePic':
-        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+        'https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg'
   },
   {
     'name': 'Joseph',
     'message': 'I am from International Olym...',
     'time': '6:02 am',
     'profilePic':
-        'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+        'https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg'
   },
   {
     'name': 'Sikandar',
     'message': 'Lets Code!',
     'time': '4:56 am',
     'profilePic':
-        'https://images.unsplash.com/photo-1619194617062-5a61b9c6a049?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
+        'https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg'
   },
   {
     'name': 'Ian Dooley',
     'message': 'Images by Unsplash',
     'time': '1:00 am',
     'profilePic':
-        'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+        'https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg'
   },
 ];
 
