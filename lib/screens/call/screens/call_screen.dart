0791b4158a091866ea244e650c398fab4bce6709
Mateen <mateendev3@gmail.com>
2022-10-11 21:42:44 +0500

    Added agora with heroku configurations

--- lib/screens/call/screens/call_screen.dart
+++ lib/screens/call/screens/call_screen.dart
@@ -1,6 +1,8 @@
+import 'package:agora_uikit/agora_uikit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:lets_chat/utils/common/config/agora_config.dart';
+import 'package:lets_chat/utils/common/widgets/loader.dart';
 import '../../../models/call.dart';
 
 class CallScreen extends ConsumerStatefulWidget {
@@ -19,8 +21,40 @@ class CallScreen extends ConsumerStatefulWidget {
 }
 
 class _CallScreenState extends ConsumerState<CallScreen> {
+  AgoraClient? client;
+
+  @override
+  void initState() {
+    super.initState();
+
+    client = AgoraClient(
+      agoraConnectionData: AgoraConnectionData(
+        appId: AgoraConfig.appCertificate,
+        channelName: widget.channelId,
+        tokenUrl: AgoraConfig.tokenBaseUrl,
+      ),
+    );
+
+    initAgora();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: client == null
+          ? const Loader()
+          : SafeArea(
+              child: Stack(
+                children: [
+                  AgoraVideoViewer(client: client!),
+                  AgoraVideoButtons(client: client!),
+                ],
+              ),
+            ),
+    );
+  }
+
+  void initAgora() async {
+    await client!.initialize();
   }
 }
--- lib/utils/common/config/agora_config.dart
+++ lib/utils/common/config/agora_config.dart
@@ -2,4 +2,6 @@ class AgoraConfig {
   static const String token = '';
   static const String appId = '1d314449ddda4e2ea9bdf0bc1342b910';
   static const String appCertificate = '9f833d0abe0745b5a2a30662be89c6d7';
+  static const String tokenBaseUrl =
+      'https://lets-chat-mateendev3.herokuapp.com/';
 }
